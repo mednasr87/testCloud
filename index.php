bd7a7421940d0141652b58b0b97e4bb891bfd8f2
mednasr87 <53326033+mednasr87@users.noreply.github.com>
2024-10-21 16:35:42 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+	$conn = mysqli_init();
+	mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/cert.pem", NULL, NULL);
+	mysqli_real_connect($conn, "essatdbcloud2024.mysql.database.azure.com", "mednasr", "Essat30083974..", "test", 3306, MYSQLI_CLIENT_SSL);
+        if($conn->connect_error){
+                die('Erreur : ' .$conn->connect_error);
+        } 
+        
+
+
+echo "<h1> Ceci est mon premier site web</h1>";
+
+$result = $conn->query("select * from personne;");
+foreach ($result as $row) {
+    echo " id = " . $row['ID_Personne'] . "nom= ".$row['nom']."\n";
+}
+
+
+?>
